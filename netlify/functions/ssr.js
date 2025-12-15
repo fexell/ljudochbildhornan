d4ffa6ecd8ae3a32e61009d21a43c4fa41dc42ee
Felix Cervin <fexellofficial@gmail.com>
2025-12-15 10:04:49 +0100

    Commit

--- netlify/functions/ssr.js
+++ netlify/functions/ssr.js
@@ -1,3 +1,27 @@
-import handler from '../../dist/server/index.js'
+import * as app from '../../dist/server/index.js';
 
-export default handler
+export async function handler(event, context) {
+  const request = new Request(
+    event.rawUrl,
+    {
+      method: event.httpMethod,
+      headers: event.headers,
+      body:
+        event.httpMethod === 'GET' || event.httpMethod === 'HEAD'
+          ? undefined
+          : event.body,
+    },
+  );
+
+  const response = await app.fetch(
+    request,
+    process.env,
+    context,
+  );
+
+  return {
+    statusCode: response.status,
+    headers: Object.fromEntries(response.headers),
+    body: await response.text(),
+  };
+}
