39b069762f187dda023fd571dddcfc6b668ff387
Felix Cervin <fexellofficial@gmail.com>
2025-12-15 12:09:36 +0100

    Commit

---
+++ netlify/functions/ssr.js
@@ -0,0 +1,31 @@
+// netlify/functions/ssr.js
+import * as build from '../../dist/server/index.js';
+import {createRequestHandler} from '@shopify/hydrogen';
+
+// Create Hydrogen request handler
+const handleRequest = createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV,
+});
+
+export async function handler(event) {
+  // Convert Netlify event to Fetch API Request
+  const request = new Request(event.rawUrl, {
+    method: event.httpMethod,
+    headers: event.headers,
+    body:
+      event.httpMethod === 'GET' || event.httpMethod === 'HEAD'
+        ? undefined
+        : event.body,
+  });
+
+  // Call Hydrogen handler
+  const response = await handleRequest(request);
+
+  // Convert Fetch Response to Netlify response
+  return {
+    statusCode: response.status,
+    headers: Object.fromEntries(response.headers),
+    body: await response.text(),
+  };
+}
